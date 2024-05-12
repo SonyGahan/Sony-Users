f6c9f37e38290e3c3b58a7b586a035d3b9f8009f
SonyGahan <94231488+SonyGahan@users.noreply.github.com>
2024-05-12 19:18:54 -0300

    [FEATURE] Pruebas de CRUD funcionando correctamente

--- src/main/java/com/sonyairlines/user/Model/User.java
+++ src/main/java/com/sonyairlines/user/Model/User.java
@@ -18,18 +18,12 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nombre;
-    private String apellido;
-    private String fechaNac;
-    private int dni;
     private String email;
     private String passport;
 
     //Contructor
-    public User(String nombre, String apellido, String fechaNac, int dni, String email, String passport) {
+    public User(String nombre, String email, String passport) {
         this.nombre = nombre;
-        this.apellido = apellido;
-        this.fechaNac = fechaNac;
-        this.dni = dni;
         this.email = email;
         this.passport = passport;
     }
